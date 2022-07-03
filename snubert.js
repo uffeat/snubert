9ea112345fdc2f285bd45985a35c622280420f0e
Uffe <uat@aperitas.com>
2022-07-03 19:33:45 +0200

    w

--- snubert.js
+++ snubert.js
@@ -1,52 +1,54 @@
-import * as components from './components/all/all.js'
-import { modal } from './components/modal.js'
-import { callWorker } from './utils/call-worker.js'
-import { createElement } from './utils/elements.js'
-import { focus } from './utils/focus.js'
-import { states } from './utils/states.js'
-import { storage } from './utils/storage.js'
-import { theme } from './utils/theme.js'
+import * as components from './components/all/all.js';
+import { modal } from './components/modal.js';
+import { callWorker } from './utils/call-worker.js';
+import { createElement } from './utils/elements.js';
+import { focus } from './utils/focus.js';
+import { states } from './utils/states.js';
+import { storage } from './utils/storage.js';
+import { theme } from './utils/theme.js';
+
+console.log("Executing 'snubert' module...")
 
 /* Class for the 'snubert' namespace. */
 class Snubert {
-  #eRoot
+  #eRoot;
   constructor() {
-    this.callWorker = callWorker
-    this.components = components
-    this.createElement = createElement
-    this.focus = focus
-    this.modal = modal
-    this.states = states
-    this.storage = storage
-    this.theme = theme
+    this.callWorker = callWorker;
+    this.components = components;
+    this.createElement = createElement;
+    this.focus = focus;
+    this.modal = modal;
+    this.states = states;
+    this.storage = storage;
+    this.theme = theme;
   }
 
   get eRoot() {
     if (!this.#eRoot) {
-      throw new Error(`Root element not set. Use 'setRoot(element)' to set root element.`)
+      throw new Error(`Root element not set. Use 'setRoot(element)' to set root element.`);
     }
-    return this.#eRoot
+    return this.#eRoot;
   }
 
   set eRoot(value) {
-    throw new Error(`Property 'eRoot' is read-only. Use 'setRoot(element)' to set root element.`)
+    throw new Error(`Property 'eRoot' is read-only. Use 'setRoot(element)' to set root element.`);
   }
 
   createComponent(Component, properties, kwargs = {}) {
-    return new window.snubert.components[Component](properties, kwargs)
+    return new window.snubert.components[Component](properties, kwargs);
   }
 
   setRoot(element) {
     if (!element) {
-      console.info(`Creating root component...`)
-      element = this.createComponent('Root')
-      document.body.append(element)
+      console.info(`Creating root component...`);
+      element = this.createComponent('Root');
+      document.body.append(element);
     }
-    this.#eRoot = element
+    this.#eRoot = element;
   }
 
 }
 
 
-const snubert = new Snubert()
-window.snubert = snubert
+const snubert = new Snubert();
+window.snubert = snubert;
