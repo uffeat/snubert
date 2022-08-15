fe54d79c271a50584a61dab6f961f95e6fc46521
Uffe <uat@aperitas.com>
2022-08-15 10:38:42 +0200

    w

--- components/mixins/attrs.js
+++ components/mixins/attrs.js
@@ -1,7 +1,7 @@
 /* Component mixin for managing attributes incl. sync with properties. */
-const MixinAttrs = Parent => {
+const MixinAttrs = (Parent) => {
   class Attrs extends Parent {
-    static requiredMixins = ['Props'];  // Applied in mixin function (mixin.js).
+    static requiredMixins = ["Props"]; // Applied in mixin function (mixin.js).
     constructor() {
       super();
     }
@@ -10,7 +10,7 @@ const MixinAttrs = Parent => {
     attributeChangedCallback(attr, oldValue, newValue) {
       // this._noSyncAttributes should be an array containing any attributes that should escape sync with properties.
       if (this._noSyncAttributes?.includes(attr)) {
-        return
+        return;
       }
       /* 
       attribute -> property value interpretation logic:
@@ -24,46 +24,61 @@ const MixinAttrs = Parent => {
         return;
       }
       // Get property name that correponds to attr (kebab -> camel):
-      const prop = attr.split('-').map((w, i) => i ? w[0].toUpperCase() + w.slice(1) : w).join('');
+      const prop = attr
+        .split("-")
+        .map((w, i) => (i ? w[0].toUpperCase() + w.slice(1) : w))
+        .join("");
       // Determine value suitable for setting prop:
       // Initially, assume that a value attributte (not representing a number) has been changed:
       let propValue = newValue;
       // ... challenge this initial assumption:
-      if (oldValue === null && newValue === '') {
+      if (oldValue === null && newValue === "") {
         // A no-value attributte has been added. This corresponds to a true property value:
         propValue = true;
-      }
-      else if (oldValue === '' && newValue === null) {
+      } else if (oldValue === "" && newValue === null) {
         // A no-value attributte has been removed. This corresponds to a false property value:
         propValue = false;
-      }
-      else if (isNaN(newValue) === false) {
+      } else if (isNaN(newValue) === false) {
         // A value attributte that represents a number has been changed.
         propValue = Number(newValue);
       }
       if (this.updateProperty) {
         this.updateProperty(prop, propValue);
-      }
-      else {
+      } else {
         console.warn(`Component has no 'updateProperty' method. Property '${prop}' not updated following attribute change.
         To fix this problem, add 'MixinPops' to the component.`);
       }
-      
     }
 
     /* Returns and validates observed attr that correponds to a given property (camel -> kebab). */
     getObservedAttribute(prop) {
       /* Helper function: Returns true if character c is upper case or contains an integer; otherwise returns false. */
-      const isUpperOrInt = c => {
+      const isUpperOrInt = (c) => {
         const isUpper = c.charCodeAt(0) >= 65 && c.charCodeAt(0) <= 90;
-        const isNumber = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'].includes(c);
+        const isNumber = [
+          "0",
+          "1",
+          "2",
+          "3",
+          "4",
+          "5",
+          "6",
+          "7",
+          "8",
+          "9",
+        ].includes(c);
         return isUpper || isNumber;
-      }
+      };
       // Get attribute name by camel -> kebab interpretation:
-      const attr = prop.split('').map((c) => isUpperOrInt(c) ? '-' + c.toLowerCase() : c).join('');
+      const attr = prop
+        .split("")
+        .map((c) => (isUpperOrInt(c) ? "-" + c.toLowerCase() : c))
+        .join("");
       // Verify that attribute is observed:
       if (!this.constructor.observedAttributes?.includes(attr)) {
-        throw new Error(`Property '${prop}' has no corresponding observed attribute ('${attr}').`);
+        throw new Error(
+          `Property '${prop}' has no corresponding observed attribute ('${attr}').`
+        );
       }
       return attr;
     }
@@ -72,18 +87,15 @@ const MixinAttrs = Parent => {
     // Helper method for property setters (for incomming arg and/or before setting correponsding attribute).
     interpretValue(value, interpretation) {
       // String -> Boolean, null:
-      if (interpretation === 'toBoolean') {
-        if (value === 'true') {
+      if (interpretation === "toBoolean") {
+        if (value === "true") {
           return true;
-        }
-        else if (value === 'false') {
+        } else if (value === "false") {
           return false;
-        }
-        else if (value === 'null') {
+        } else if (value === "null") {
           return null;
         }
-      }
-      else {
+      } else {
         throw new Error(`interpretation '${interpretation}' not supported.`);
       }
       return value;
@@ -92,12 +104,11 @@ const MixinAttrs = Parent => {
     /* Sets or removes no-value attribute from Boolean/null/undefined (property) value. */
     setNoValueAttribute(attr, value) {
       if (value === true) {
-        this.setAttribute(attr, '');
-      }
-      else if (value === false) {  // false/null/undefined.
+        this.setAttribute(attr, "");
+      } else if (value === false) {
+        // false/null/undefined.
         this.removeAttribute(attr);
-      }
-      else {
+      } else {
         throw new Error(`Expected Boolean, null or undefined. Got '${value}';`);
       }
     }
@@ -108,19 +119,16 @@ const MixinAttrs = Parent => {
       const attr = this.getObservedAttribute(prop);
       if (!interpretation) {
         this.setAttribute(attr, value);
-      }
-      else if (interpretation === 'noValue') {
+      } else if (interpretation === "noValue") {
         this.setNoValueAttribute(attr, value);
-      }
-      else {
+      } else {
         throw new Error(`Interpretation '${interpretation}' not implemented.`);
       }
     }
-
   }
   // Named class is returned explicitly (rather than 'return class...' in the beginning of the mixin function)
   // to allow for any processing of the class before return.
   return Attrs;
-}
+};
 
 export { MixinAttrs };
